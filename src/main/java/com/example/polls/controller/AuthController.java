3566d06525ba57318fc740c7ad61cb89807b24d2
hakob <hakobhn@yahoo.com>
2019-07-30 23:12:45 +0400

    auth functionality

--- src/main/java/com/example/polls/controller/AuthController.java
+++ src/main/java/com/example/polls/controller/AuthController.java
@@ -55,10 +55,10 @@ public class AuthController {
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginRequest.getUsernameOrEmail(),
-                        loginRequest.getPassword()
-                )
+            new UsernamePasswordAuthenticationToken(
+                    loginRequest.getUsernameOrEmail(),
+                    loginRequest.getPassword()
+            )
         );
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
