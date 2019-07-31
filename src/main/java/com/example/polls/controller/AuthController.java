58cddea7ec0dd6de0e7bc6814c436b06255e3677
hakob <hakobhn@yahoo.com>
2019-08-01 01:40:05 +0400

    auth functionality

--- src/main/java/com/example/polls/controller/AuthController.java
+++ src/main/java/com/example/polls/controller/AuthController.java
@@ -65,6 +65,27 @@ public class AuthController {
         return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
     }
 
+    @PostMapping("/m/signin")
+    public ResponseEntity<?> authenticateMobileUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsernameOrEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.generateToken(authentication);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/users/{username}")
+                .buildAndExpand(loginRequest.getUsernameOrEmail()).toUri();
+
+        return ResponseEntity.created(location).body(new ApiResponse(true, "User signed up successfully", jwt));
+    }
+
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
         if (userRepository.existsByUsername(signUpRequest.getUsername())) {
@@ -89,11 +110,55 @@ public class AuthController {
         user.setRoles(Collections.singleton(userRole));
 
         User result = userRepository.save(user);
+        user.setPassword("");
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentContextPath().path("/users/{username}")
                 .buildAndExpand(result.getUsername()).toUri();
 
         return ResponseEntity.created(location).body(new ApiResponse(true, "User registered successfully"));
+    }
+
+    @PostMapping("/m/signup")
+    public ResponseEntity<?> registerMobileUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return new ResponseEntity(new ApiResponse(false, "Username is already taken!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return new ResponseEntity(new ApiResponse(false, "Email Address already in use!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // Creating user's account
+        User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
+                signUpRequest.getEmail(), signUpRequest.getPassword());
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new AppException("User Role not set."));
+
+        user.setRoles(Collections.singleton(userRole));
+
+        User result = userRepository.save(user);
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        result.getUsername(),
+                        signUpRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.generateToken(authentication);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/users/{username}")
+                .buildAndExpand(result.getUsername()).toUri();
+
+        return ResponseEntity.created(location).body(new ApiResponse(true, "User registered successfully", jwt));
     }
 }
--- src/main/java/com/example/polls/payload/ApiResponse.java
+++ src/main/java/com/example/polls/payload/ApiResponse.java
@@ -1,5 +1,7 @@
 package com.example.polls.payload;
 
+import org.springframework.lang.Nullable;
+
 /**
  * Created by rajeevkumarsingh on 19/08/17.
  */
@@ -7,9 +9,18 @@ public class ApiResponse {
     private Boolean success;
     private String message;
 
+    @Nullable
+    private Object body;
+
     public ApiResponse(Boolean success, String message) {
         this.success = success;
         this.message = message;
+    }
+
+    public ApiResponse(Boolean success, String message, Object body) {
+        this.success = success;
+        this.message = message;
+        this.body = body;
     }
 
     public Boolean getSuccess() {
@@ -26,5 +37,14 @@ public class ApiResponse {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    @Nullable
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(@Nullable Object body) {
+        this.body = body;
     }
 }
